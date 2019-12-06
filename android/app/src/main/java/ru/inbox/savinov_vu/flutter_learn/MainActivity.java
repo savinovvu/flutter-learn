c0c7fea73125d9291346dea2d9847ddd4d712b78
Savinov Vasiliy <savinov_vu@inbox.ru>
2019-12-07 07:58:53 +0800

    1. initial commit

---
+++ android/app/src/main/java/ru/inbox/savinov_vu/flutter_learn/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.inbox.savinov_vu.flutter_learn;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
