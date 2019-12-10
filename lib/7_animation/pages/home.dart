f1059fecea45ae804bf3232680d9b26ddfdb31bc
Savinov Vasiliy <savinov_vu@inbox.ru>
2019-12-10 13:12:54 +0800

    7. animation

---
+++ lib/7_animation/pages/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn/7_animation/widgets/animated_container.dart';
+import 'package:flutter_learn/7_animation/widgets/animated_cross_fade.dart';
+import 'package:flutter_learn/7_animation/widgets/animated_opacity.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            AnimatedContainerWidget(),
+            Divider(),
+            AnimatedCrossFadeWidget(),
+            Divider(),
+            AnimatedOpacityWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
