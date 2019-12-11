953838497fb07b8a3bf4f86354521f7a32cf344c
Savinov Vasiliy <savinov_vu@inbox.ru>
2019-12-11 11:17:13 +0800

    8. navigatino

--- lib/13_side_bar_navigation/pages/home.dart
+++ lib/13_side_bar_navigation/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/13_side_bar_navigation/widgets/left_drawer.dart';
+import 'package:flutter_learn/13_side_bar_navigation/widgets/right_drawer.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,9 +12,13 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Drawer'),
       ),
-      body: SafeArea(),
+      drawer: const LeftDrawerWidget(),
+      endDrawer: const RightDrawerWidget(),
+      body: SafeArea(
+        child: Container(),
+      ),
     );
   }
 }
