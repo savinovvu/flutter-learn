1212cbe91745c73c70760747101bb482e66e75eb
Savinov Vasiliy <savinov_vu@inbox.ru>
2019-12-11 13:46:34 +0800

    8. navigatino

---
+++ lib/14_listview/pages/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn/14_listview/widgets/header.dart';
+import 'package:flutter_learn/14_listview/widgets/row.dart';
+import 'package:flutter_learn/14_listview/widgets/row_with_cart.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return HeaderWidget();
+            } else if (index >= 1 && index <= 3) {
+              return RowWithCardWidget(index: index);
+            } else {
+              return RowWidget(index: index);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
